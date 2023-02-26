a553ac8d65c265edd270196bf6790526a0f3cdb4
nabbasi <nabbasi@qterminals.com>
2023-02-26 10:54:03 +0300

    Refactor application to build and deploy in docker container

--- expressjs/backend/deploy/build-docker-image.sh
+++ expressjs/backend/deploy/build-docker-image.sh
@@ -1,3 +1,3 @@
-docker rmi image exporess/prodction
-docker build -t "exporess/prodction:latest" .
-docker run --rm --name nodejs -it -p8000:8000 exporess/prodction:latest
+docker rmi image express/prodction
+docker build --progress=plain --no-cache -t "express/prodction:latest" .
+docker run --rm --name nodejs -it -p8000:8000 express/prodction:latest
