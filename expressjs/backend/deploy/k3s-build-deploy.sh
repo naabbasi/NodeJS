bd6cbb018f82a8bde0c96073545577a02fe10f1a
nabbasi <nabbasi@qterminals.com>
2023-02-27 12:34:44 +0300

    Refactor application to build and deploy in docker container

--- expressjs/backend/deploy/k3s-build-deploy.sh
+++ expressjs/backend/deploy/k3s-build-deploy.sh
@@ -1,4 +1,5 @@
 sudo docker save --output deploy_ecommerce_app-latest.tar deploy_ecommerce_app:latest
+sudo k3s ctr images rm docker.io/library/deploy_ecommerce_app:latest
 sudo k3s ctr images import deploy_ecommerce_app-latest.tar
 sudo k3s kubectl create -f kubernetes-app.yaml &
 sudo kubectl logs -l name=ecommerce-app
